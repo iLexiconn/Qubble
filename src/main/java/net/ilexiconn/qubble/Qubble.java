1c3ce38313ea9aad83ae3e49d4bafc68ee3d9503
gegy1000 <gegy1000@gmail.com>
2016-04-14 21:16:55 +0200

    Update build script

--- src/main/java/net/ilexiconn/qubble/Qubble.java
+++ src/main/java/net/ilexiconn/qubble/Qubble.java
@@ -7,10 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Qubble.MODID, name = "Qubble", version = Qubble.VERSION, dependencies = "required-after:llibrary@[1.2.1,)")
+@Mod(modid = Qubble.MODID, name = "Qubble", version = Qubble.VERSION, dependencies = "required-after:llibrary@[" + Qubble.LLIBRARY_VERSION + ",)")
 public class Qubble {
     public static final String MODID = "qubble";
     public static final String VERSION = "0.0.0-dev";
+    public static final String LLIBRARY_VERSION = "1.2.1";
 
     @Mod.Instance(Qubble.MODID)
     public static Qubble INSTANCE;
