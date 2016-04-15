c10474dc607b4c7a1a4c3cb96875556df2be7575
gegy1000 <gegy1000@gmail.com>
2016-04-15 14:18:24 +0200

    Button

--- src/main/java/net/ilexiconn/qubble/client/ClientEventHandler.java
+++ src/main/java/net/ilexiconn/qubble/client/ClientEventHandler.java
@@ -1,9 +1,69 @@
 package net.ilexiconn.qubble.client;
 
+import net.ilexiconn.llibrary.client.*;
+import net.ilexiconn.llibrary.client.gui.ModUpdateGUI;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.util.Rectangle;
 
 @SideOnly(Side.CLIENT)
 public enum ClientEventHandler {
     INSTANCE;
+
+    @SubscribeEvent
+    public void onInitGuiPost(GuiScreenEvent.InitGuiEvent.Post event) {
+        GuiScreen gui = event.getGui();
+
+        if (gui instanceof GuiMainMenu) {
+            int offsetX = 0;
+            int offsetY = 0;
+            int baseX = gui.width / 2 + 104;
+            int baseY = gui.height / 4 + 48 + 24 * 2;
+            int buttonX = baseX + offsetX;
+            int buttonY = baseY + offsetY;
+            while (true) {
+                if (buttonX < 0) {
+                    if (offsetY <= -48) {
+                        buttonX = 0;
+                        buttonY = 0;
+                        break;
+                    } else {
+                        offsetX = 0;
+                        offsetY += 24;
+                        buttonX = baseX + offsetX;
+                        buttonY = baseY + offsetY;
+                    }
+                }
+
+                Rectangle rectangle = new Rectangle(buttonX, buttonY, 20, 20);
+                boolean intersects = false;
+                for (int i = 0; i < event.getButtonList().size(); i++) {
+                    GuiButton button = event.getButtonList().get(i);
+                    if (!intersects) {
+                        intersects = rectangle.intersects(new Rectangle(button.xPosition, button.yPosition, button.width, button.height));
+                    }
+                }
+
+                if (!intersects) {
+                    break;
+                }
+
+                buttonX += 24;
+            }
+
+            event.getButtonList().add(new GuiButton(ClientProxy.QUBBLE_BUTTON_ID, buttonX, buttonY, 20, 20, "Q"));
+        }
+    }
+
+    @SubscribeEvent
+    public void onButtonPressPre(GuiScreenEvent.ActionPerformedEvent.Pre event) {
+        if (event.getGui() instanceof GuiMainMenu && event.getButton().id == ClientProxy.QUBBLE_BUTTON_ID) {
+            event.setCanceled(true);
+        }
+    }
 }
--- src/main/java/net/ilexiconn/qubble/client/ClientProxy.java
+++ src/main/java/net/ilexiconn/qubble/client/ClientProxy.java
@@ -1,12 +1,16 @@
 package net.ilexiconn.qubble.client;
 
 import net.ilexiconn.qubble.server.ServerProxy;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends ServerProxy {
+    public static final int QUBBLE_BUTTON_ID = "QUBBLE_BUTTON_ID".hashCode();
+    public static final Minecraft MINECRAFT = Minecraft.getMinecraft();
+
     @Override
     public void onPreInit() {
         super.onPreInit();
